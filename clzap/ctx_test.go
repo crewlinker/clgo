fe2e26a5ea1097013cc09f607b57a8c0aa879540
Ad van der Veer <advanderveer@gmail.com>
2023-05-10 19:06:38 +0200

    added some tests

--- clzap/ctx_test.go
+++ clzap/ctx_test.go
@@ -27,6 +27,12 @@ var _ = Describe("context", func() {
 		DeferCleanup(app.Stop, ctx1)
 	})
 
+	It("should return false if no logger", func() {
+		logs1, ok := clzap.LoggerFromContext(ctx1)
+		Expect(ok).To(BeFalse())
+		Expect(logs1).To(BeNil())
+	})
+
 	It("should return nop logger", func() {
 		logs1 := clzap.Log(ctx1)
 		Expect(logs1).To(Equal(zap.NewNop()))
