da3f1402061c2cedd54b97184694eea799270ccb
Ad van der Veer <advanderveer@gmail.com>
2023-10-08 16:42:41 +0200

    migrater overhaul

---
+++ clpostgres/snapshot_migrater.go
@@ -0,0 +1,123 @@
+package clpostgres
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+// SnapshotPath types a string to hold the full snapshot sql.
+type SnapshotPath string
+
+// SnapshotMigrater uses a snapshot strategy to migrate.
+type SnapshotMigrater struct {
+	baseMigrater
+	snapshot []byte
+}
+
+// NewSnaphotMigrater inits the migrater.
+func NewSnaphotMigrater(
+	cfg Config,
+	logs *zap.Logger,
+	rwcfg *pgxpool.Config,
+	rocfg *pgxpool.Config,
+	snapp SnapshotPath,
+) (*SnapshotMigrater, error) {
+	var err error
+
+	mig := &SnapshotMigrater{
+		baseMigrater: baseMigrater{
+			cfg:   cfg,
+			dbcfg: rwcfg,
+			logs:  logs.Named("migrater"),
+		},
+	}
+
+	mig.snapshot, err = os.ReadFile(string(snapp))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read snapshot: %w", err)
+	}
+
+	return mig, mig.baseMigrater.init(rocfg)
+}
+
+// Migrate initializes the schema.
+func (m SnapshotMigrater) Migrate(ctx context.Context) error {
+	if err := m.baseMigrater.setup(ctx); err != nil {
+		return err
+	}
+
+	tmpf, err := os.CreateTemp("", "snapshot_migrater_*")
+	if err != nil {
+		return fmt.Errorf("failed to create temp file: %w", err)
+	}
+
+	defer tmpf.Close()
+
+	if _, err := io.Copy(tmpf, bytes.NewBuffer(m.snapshot)); err != nil {
+		return fmt.Errorf("failed to write snapshot to temp file: %w", err)
+	}
+
+	exe, err := exec.LookPath("psql")
+	if err != nil {
+		return fmt.Errorf("failed to lookup 'psql': %w", err)
+	}
+
+	errb := bytes.NewBuffer(nil)
+
+	cmd := exec.CommandContext(ctx,
+		exe,
+		"-h", m.dbcfg.ConnConfig.Host,
+		"-p", fmt.Sprint(m.dbcfg.ConnConfig.Port),
+		"-U", m.dbcfg.ConnConfig.User,
+		"-d", m.dbcfg.ConnConfig.Database,
+		"-a", "-f", tmpf.Name())
+	cmd.Env = []string{"PGPASSWORD=" + m.dbcfg.ConnConfig.Password}
+	cmd.Stderr = errb
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to execute: %w: %s", err, errb.String())
+	}
+
+	if err := os.Remove(tmpf.Name()); err != nil {
+		return fmt.Errorf("failed to remove temp file: %w", err)
+	}
+
+	return nil
+}
+
+// Reset drops the schema.
+func (m SnapshotMigrater) Reset(ctx context.Context) error {
+	return m.baseMigrater.reset(ctx)
+}
+
+// SnapshotMigrated configures the di for using snapshot migrations.
+func SnapshotMigrated(sqlFile string) fx.Option {
+	return fx.Options(
+		fx.Supply(SnapshotPath(sqlFile)),
+
+		// Provide migrater which will now always run before connecting using versioned steps
+		fx.Provide(fx.Annotate(
+			NewSnaphotMigrater,
+			fx.As(new(Migrater)),
+			fx.OnStart(func(ctx context.Context, m Migrater) error { return m.Migrate(ctx) }), //nolint:wrapcheck
+			fx.OnStop(func(ctx context.Context, m Migrater) error { return m.Reset(ctx) }),    //nolint:wrapcheck
+			fx.ParamTags(``, ``, `name:"rw"`, `name:"ro"`)),
+		),
+
+		// For tests we want temporary database and auto-migratino
+		fx.Decorate(func(c Config) Config {
+			c.TemporaryDatabase = true
+			c.AutoMigration = true
+
+			return c
+		}),
+	)
+}
---
+++ clpostgres/version_migrater.go
@@ -0,0 +1,154 @@
+package clpostgres
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"ariga.io/atlas/sql/migrate"
+	"ariga.io/atlas/sql/postgres"
+	"ariga.io/atlas/sql/sqltool"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+// alwaysValidateMigrateDir can be provided as a migration dir to disable checksum validation. This
+// is useful for unit tests that don't need this feature for quick iteration.
+type alwaysValidateMigrateDir struct{ *sqltool.GolangMigrateDir }
+
+// Checksum implements the logic for re-calculate the directories checksum. But instead we return
+// the checksum in the checksum file to make the validation logic always pass.
+func (dir alwaysValidateMigrateDir) Checksum() (migrate.HashFile, error) {
+	file, err := dir.Open(migrate.HashFileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open hash file: %w", err)
+	}
+
+	defer file.Close()
+
+	byt, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read all from hash file: %w", err)
+	}
+
+	var fh migrate.HashFile
+	if err := fh.UnmarshalText(byt); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal hash file: %w", err)
+	}
+
+	return fh, nil
+}
+
+// VersionMigrated configures the di for testing with a temporary database and auto-migration of a directory.
+func VersionMigrated(migrationDir string, disableValidation bool) fx.Option {
+	return fx.Options(
+
+		// Provide migrater which will now always run before connecting using versioned steps
+		fx.Provide(fx.Annotate(
+			NewVersionMigrater,
+			fx.As(new(Migrater)),
+			fx.OnStart(func(ctx context.Context, m Migrater) error { return m.Migrate(ctx) }), //nolint:wrapcheck
+			fx.OnStop(func(ctx context.Context, m Migrater) error { return m.Reset(ctx) }),    //nolint:wrapcheck
+			fx.ParamTags(``, ``, `name:"rw"`, `name:"ro"`)),
+		),
+
+		// For tests we want temporary database and auto-migratino
+		fx.Decorate(func(c Config) Config {
+			c.TemporaryDatabase = true
+			c.AutoMigration = true
+
+			return c
+		}),
+		// provide the optional configuration for a migration dir
+		fx.Provide(func() (migrate.Dir, error) {
+			dir, err := sqltool.NewGolangMigrateDir(migrationDir)
+			if err != nil {
+				return nil, fmt.Errorf("failed to init golang migrate dir: %w", err)
+			}
+
+			if disableValidation {
+				return alwaysValidateMigrateDir{GolangMigrateDir: dir}, nil
+			}
+
+			return dir, nil
+		}),
+	)
+}
+
+// VersionMigrater allows programmatic migration of a database schema using versioned sql steps. Mostly used
+// in testing and local development to provide fully isolated databases.
+type VersionMigrater struct {
+	baseMigrater
+
+	dir migrate.Dir
+}
+
+// NewVersionMigrater inits the migrater.
+func NewVersionMigrater(
+	cfg Config,
+	logs *zap.Logger,
+	rwcfg *pgxpool.Config,
+	rocfg *pgxpool.Config,
+	dir migrate.Dir,
+) (*VersionMigrater, error) {
+	mig := &VersionMigrater{
+		baseMigrater: baseMigrater{
+			cfg:   cfg,
+			dbcfg: rwcfg,
+			logs:  logs.Named("migrater"),
+		}, dir: dir,
+	}
+
+	return mig, mig.baseMigrater.init(rocfg)
+}
+
+// Migrate initializes the schema.
+func (m VersionMigrater) Migrate(ctx context.Context) error {
+	if err := m.baseMigrater.setup(ctx); err != nil {
+		return err
+	}
+
+	if !m.cfg.AutoMigration {
+		m.logs.Info("auto-migration disabled, expect database to be provisioned already")
+
+		return nil // without auto-migration enabled, there is nothing left to do
+	}
+
+	checksum, err := m.dir.Checksum()
+	if err != nil {
+		return fmt.Errorf("failed to determine local dir checksum: %w", err)
+	}
+
+	m.logs.Info("auto-migrating from migrate dir",
+		zap.String("checksum", checksum.Sum()))
+
+	sqldb := stdlib.OpenDB(*m.dbcfg.ConnConfig)
+	defer sqldb.Close()
+
+	if err := sqldb.PingContext(ctx); err != nil {
+		return fmt.Errorf("failed to ping connection: %w", err)
+	}
+
+	drv, err := postgres.Open(sqldb)
+	if err != nil {
+		return fmt.Errorf("failed to init atlas driver: %w", err)
+	}
+
+	exec, err := migrate.NewExecutor(drv, m.dir, migrate.NopRevisionReadWriter{})
+	if err != nil {
+		return fmt.Errorf("failed to init executor: %w", err)
+	}
+
+	if err := exec.ExecuteN(ctx, -1); err != nil {
+		return fmt.Errorf("failed to execute migrations: %w", err)
+	}
+
+	return nil
+}
+
+// Reset drops the migrated state.
+func (m VersionMigrater) Reset(ctx context.Context) error {
+	return m.baseMigrater.reset(ctx)
+}
