aa51c49528110e38e8325fdeaf087d67641e6305
Ad van der Veer <advanderveer@gmail.com>
2023-11-29 13:26:11 +0100

    added to cdk

--- clcdk/postgres_test.go
+++ clcdk/postgres_test.go
@@ -1,6 +1,9 @@
 package clcdk_test
 
 import (
+	"bytes"
+	"encoding/json"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
@@ -31,5 +34,24 @@ var _ = Describe("postgres", func() {
 		tmpl.ResourceCountIs(jsii.String("AWS::RDS::DBInstance"), jsii.Number(1))
 		tmpl.ResourceCountIs(jsii.String("AWS::RDS::DBParameterGroup"), jsii.Number(1))
 		tmpl.ResourceCountIs(jsii.String("AWS::EC2::SecurityGroup"), jsii.Number(1))
+	})
+
+	It("should create custom resource provider", func() {
+		vpc := clcdk.WithNetwork(stack, "Network1", cfg)
+		db, dbSecret := clcdk.WithPostgresInstance(stack, "Postgres1", cfg, vpc)
+		clcdk.WithPostgresCustomResources(stack, "PgCustom1", cfg, db, dbSecret)
+
+		tmpl := assertions.Template_FromStack(stack, nil)
+
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.SetIndent("", " ")
+		enc.Encode(tmpl.ToJSON())
+
+		tmpl.ResourceCountIs(jsii.String("AWS::Lambda::Function"), jsii.Number(3))
+
+		tmpl.HasResourceProperties(jsii.String("AWS::Lambda::Function"), map[string]any{
+			"Description": "AWS CDK resource provider framework - onEvent (Stack1/PgCustom1/Provider)",
+		})
 	})
 })
