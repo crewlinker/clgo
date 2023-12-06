6f3e3c29b27c0a52884033c3c27af44ed26e957a
Ad van der Veer <advanderveer@gmail.com>
2023-12-06 16:07:17 +0100

    cleaned up old ref code

--- clcdk/refs.go
+++ clcdk/refs.go
@@ -1,11 +1,8 @@
 package clcdk
 
 import (
-	"strings"
-
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
 )
 
 type tokenRef struct {
@@ -16,62 +13,14 @@ func (r tokenRef) ImportValue() *string {
 	return r.token
 }
 
-func (r tokenRef) Dereference() *string {
-	return awscdk.Fn_ImportValue(r.token)
-}
-
-// StdRef replaces [Ref] and uses the CDK native tooling for exporting values.
-type StdRef interface {
-	// Dereference the value carried by this reference. It results in the use
-	// of Fn::ImportValue.
-	Dereference() *string
-	// ImportValue will use the ref value through the user of Fn:ImportValue
+// Ref represents a value that can be imported in another stack.
+type Ref interface {
+	// ImportValue will use the ref value through the use of Fn:ImportValue
 	ImportValue() *string
 }
 
 // ExportValue uses the CDK's native method on the stack to export any 'v' that
-// is a construct property. It replaces "Export".
-func ExportValue(scope constructs.Construct, v any) StdRef {
+// is a construct property.
+func ExportValue(scope constructs.Construct, v any) Ref {
 	return tokenRef{awscdk.Stack_Of(scope).ExportValue(v, nil)}
-}
-
-// Ref can be passed between stacks to easy imports and exports.
-type Ref interface {
-	Import(scope constructs.Construct) *string
-}
-
-type ref struct {
-	ident  string
-	source awscdk.Stack
-}
-
-// Import the referenced value. Will also create a dependency from the stack of 'scope' ON the
-// stack that exported the reference.
-func (r ref) Import(scope constructs.Construct) *string {
-	awscdk.Stack_Of(scope).AddDependency(r.source, jsii.String("reference: "+r.ident))
-
-	return awscdk.Fn_ImportValue(jsii.String(r.ident))
-}
-
-// Export a value from the stack owning 'scope' in a standardized way.
-//
-// Deprecated: Export is deprecated, instead "ExportValue" should be used.
-func Export(scope constructs.Construct, name ScopeName, conv Conventions, val *string) Ref {
-	stack := awscdk.Stack_Of(scope)
-	scope = name.ChildScope(scope)
-	scope.ToString()
-
-	desc := conv.Qualifier() + *scope.ToString()
-	ident := strings.ReplaceAll(desc, "/", "")
-
-	awscdk.NewCfnOutput(scope, jsii.String("Export"), &awscdk.CfnOutputProps{
-		Value:       val,
-		Description: scope.Node().Path(),
-		ExportName:  jsii.String(ident),
-	})
-
-	return ref{
-		ident:  ident,
-		source: stack,
-	}
 }
--- clcdk/refs_test.go
+++ clcdk/refs_test.go
@@ -17,37 +17,12 @@ import (
 
 var _ = Describe("refs", func() {
 	var app awscdk.App
-	var conv clcdk.Conventions
 
 	BeforeEach(func() {
 		app = awscdk.NewApp(nil)
-		conv = clcdk.NewConventions("ClFoo", "eu-west-1", "111111")
 	})
 
-	It("should create an instanced stack with instance context", func() {
-		stack1 := awscdk.NewStack(app, jsii.String("Stack1"), nil)
-		stack2 := awscdk.NewStack(app, jsii.String("Stack2"), nil)
-
-		scope := clcdk.ScopeName("Scope1").ChildScope(stack1)
-		scope = clcdk.ScopeName("Scope2").ChildScope(scope)
-
-		ref1 := clcdk.Export(scope, "MyExport", conv, stack1.StackName())
-		ref1.Import(stack2)
-
-		tmpl1 := assertions.Template_FromStack(stack1, nil)
-		map1 := *tmpl1.ToJSON()
-		Expect(map1["Outputs"]).To(HaveLen(1))
-
-		json1 := lo.Must(json.Marshal(map1["Outputs"]))
-		Expect(json1).To(ContainSubstring(`"Description":"Stack1/Scope1/Scope2/MyExport"`))
-		Expect(json1).To(ContainSubstring(`"Export":{"Name":"ClFooStack1Scope1Scope2MyExport"}`))
-		Expect(json1).To(ContainSubstring(`"Value":"Stack1"`))
-
-		Expect(*stack2.Dependencies()).To(HaveLen(1))
-		Expect(*(*stack2.Dependencies())[0].StackName()).To(Equal("Stack1"))
-	})
-
-	It("stdref", func() {
+	It("export import", func() {
 		By("exporting it from a stack")
 		stack1 := awscdk.NewStack(app, jsii.String("Stack1"), nil)
 		bucket1 := awss3.NewBucket(stack1, jsii.String("Bucket1"), nil)
