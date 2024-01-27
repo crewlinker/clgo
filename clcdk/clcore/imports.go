57215a7b248cd1eb3ac8a843b546730a2f093128
Ad van der Veer <advanderveer@gmail.com>
2024-01-27 12:56:14 +0100

    added cl core imports

---
+++ clcdk/clcore/imports.go
@@ -0,0 +1,89 @@
+package clcore
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsecs"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type imports struct {
+	vpc     awsec2.IVpc
+	cluster awsecs.ICluster
+}
+
+// Imports describe resources imported from the "core" stack.
+type Imports interface {
+	VPC() awsec2.IVpc
+	Cluster() awsecs.ICluster
+}
+
+const (
+	nrOfZones = 2
+)
+
+// NewImports inits imported resources from the "Core".
+func NewImports(scope constructs.Construct, importPrefix string) Imports {
+	con := imports{}
+
+	con.vpc = awsec2.Vpc_FromVpcAttributes(scope, jsii.String("Vpc"), &awsec2.VpcAttributes{
+		VpcId: awscdk.Fn_ImportValue(jsii.String(importPrefix + ":VpcId")),
+		AvailabilityZones: awscdk.Fn_Split(jsii.String(","),
+			awscdk.Fn_ImportValue(jsii.String(importPrefix+":AvailabilityZones")), jsii.Number(nrOfZones)),
+		PublicSubnetIds: awscdk.Fn_Split(jsii.String(","),
+			awscdk.Fn_ImportValue(jsii.String(importPrefix+":PublicSubnetIds")), jsii.Number(nrOfZones)),
+		PublicSubnetRouteTableIds: awscdk.Fn_Split(jsii.String(","),
+			awscdk.Fn_ImportValue(jsii.String(importPrefix+":PublicSubnetRtIds")), jsii.Number(nrOfZones)),
+	})
+
+	con.cluster = awsecs.Cluster_FromClusterAttributes(scope, jsii.String("Cluster"), &awsecs.ClusterAttributes{
+		ClusterName:    awscdk.Fn_ImportValue(jsii.String(importPrefix + ":ClusterName")),
+		Vpc:            con.vpc,
+		HasEc2Capacity: jsii.Bool(true),
+	})
+
+	return con
+}
+
+func (con imports) Cluster() awsecs.ICluster { return con.cluster }
+func (con imports) VPC() awsec2.IVpc         { return con.vpc }
+
+// ImportCapacityProviderName will use the importPrefix and instanceID to import its capacity provider name.
+func ImportCapacityProviderName(importPrefix, instanceID string) *string {
+	return awscdk.Fn_ImportValue(jsii.String(importPrefix + ":" + instanceID + ":CapacityProviderName"))
+}
+
+type instanceImports struct {
+	capacityProviderName *string
+	securityGroup        awsec2.ISecurityGroup
+}
+
+// InstanceImports are imports for deploying something on an instance via an
+// ECS cluster with a capacity provider.
+type InstanceImports interface {
+	CapacityProviderName() *string
+	SecurityGroup() awsec2.ISecurityGroup
+}
+
+// NewInstanceImports inits imports for a capacity instance.
+func NewInstanceImports(scope constructs.Construct, importPrefix, instanceID string) InstanceImports {
+	scope, con := constructs.NewConstruct(scope, jsii.String("InstanceImport"+instanceID)), instanceImports{}
+
+	con.capacityProviderName = awscdk.Fn_ImportValue(
+		jsii.String(importPrefix + ":" + instanceID + ":CapacityProviderName"))
+	con.securityGroup = awsec2.SecurityGroup_FromSecurityGroupId(
+		scope,
+		jsii.String("SecurityGroup"),
+		awscdk.Fn_ImportValue(jsii.String(importPrefix+":"+instanceID+":SecurityGroupId")), nil)
+
+	return con
+}
+
+func (con instanceImports) CapacityProviderName() *string {
+	return con.capacityProviderName
+}
+
+func (con instanceImports) SecurityGroup() awsec2.ISecurityGroup {
+	return con.securityGroup
+}
